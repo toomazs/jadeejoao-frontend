0cfe72ff8b91dc9684ef0ed4b6244d205b004093
Eduardo <duduclaude50@gmail.com>
2026-08-10 20:44:15 -0300

    feat: add design tokens, self-hosted fonts, and page metadata

---
+++ src/assets.ts
@@ -0,0 +1,10 @@
+/**
+ * Brand assets bundled (and hashed) by Vite from the repo's `images/` dir.
+ * Import from here instead of reaching into `images/` directly.
+ */
+
+import logoVertical from '../images/logo-vertical.png'
+import logo from '../images/logo.png'
+import siriguela from '../images/siriguela.png'
+
+export { logo, logoVertical, siriguela }
